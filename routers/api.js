a96572202589f76070774d8fdd8e65c03752e067
Douglas Binder <binder@app.fintech.io>
2015-03-05 13:48:04 -0500

    socket

--- routers/api.js
+++ routers/api.js
@@ -4,7 +4,7 @@ module.exports = function(app, express){
     var router = express.Router();
 
     router.post('/gitpush', function(req, res){
-        console.log(req)
+        console.log(req.body)
     
 })
     router.get('/', function(req, res){
