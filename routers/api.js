fb1fcba421f92ecf5e5b29c56804902e26bc138b
Douglas Binder <binder@app.fintech.io>
2015-03-05 13:53:42 -0500

    scoket

--- routers/api.js
+++ routers/api.js
@@ -4,7 +4,7 @@ module.exports = function(app, express){
     var router = express.Router();
 
     router.post('/gitpush', function(req, res){
-        console.log("body of the req=",req.body)
+        console.log("body of the req=",res)
     
 })
     router.get('/', function(req, res){
